2ae2b0a3921baa6dfc85c2566893e0a5b425b701
Chandra Tungathurthi <chandra.tungathurthi@rwth-aachen.de>
2019-09-22 15:30:00 +0200

    fixing the server config
    Signed-off-by: Chandra Tungathurthi <chandra.tungathurthi@rwth-aachen.de>

--- config/prod.exs
+++ config/prod.exs
@@ -13,10 +13,17 @@ port =
     environment variable PORT is missing.
     """
 
+host = System.get_env("HOST") || "localhost"
+
 config :dm_web_front, EfaMonitor.DmFront.Web.Endpoint,
+  url: [host: host, port: String.to_integer(port)],
   http: [:inet6, port: String.to_integer(port)],
   secret_key_base: secret_key_base,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  load_from_system_env: true,
+  server: true,
+  code_reloader: false
+
 
 config :logger, :console, format: "[$level] $message\n"
 config :logger, level: :info
